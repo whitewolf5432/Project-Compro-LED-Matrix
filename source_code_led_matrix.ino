e6ded2b774ac947771d40516d5315f60594527bf
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2021-05-18 14:46:00 +0700

    :bulb: Update source oode.

--- source_code_led_matrix.ino
+++ source_code_led_matrix.ino
@@ -1,8 +1,9 @@
-char msg[] ="IT";//Change the text here.
-int scrollspeed=10;//Set the scroll speed ( lower=faster)
-
 int x=0;
-int y;
+int index;
+String msg = "I LOVE COMPRO";//Create word.
+
+//Count character
+int length = msg.length();
 
 //Columns
 int clockPin1 = 2; //Arduino pin connected to Clock Pin 11 of 74HC595
@@ -14,19 +15,9 @@ int clockPin2 = 5; //Arduino pin connected to Clock Pin 11 of 74HC595
 int latchPin2 = 6; //Arduino pin connected to Latch Pin 12 of 74HC595
 int dataPin2 = 7;  //Arduino pin connected to Data Pin 14 of 74HC595
 
-//BITMAP
-//Bits in this array represents one LED of the matrix
-// 8 is # of rows, 8 is # of LED matrices
-byte bitmap[8][8]; 
-
-int numZones = sizeof(bitmap) / 8; // One Zone refers to one 8 x 8 Matrix ( Group of 8 columns)
-int maxZoneIndex = numZones-1;
-int numCols = numZones * 8;
-
 //FONT DEFENITION
 byte alphabets[][8] = {
- {0, 0,0,0,0,0},//@ as SPACE
- //{8,28,54,99,65},//<<
+ {0, 0, 0, 0, 0, 0, 0, 0},//SPACE
  {0, 31, 36, 68, 36, 31},//A
  {0, 127, 73, 73, 73, 54},//B
  {0, 62, 65, 65, 65, 34},//C
@@ -55,7 +46,8 @@ byte alphabets[][8] = {
  {0, 67, 69, 73, 81, 97},//Z
 };
 
-void setup() {
+void setup()
+{
  pinMode(latchPin1, OUTPUT);
  pinMode(clockPin1, OUTPUT);
  pinMode(dataPin1, OUTPUT);
@@ -63,82 +55,39 @@ void setup() {
  pinMode(latchPin2, OUTPUT);
  pinMode(clockPin2, OUTPUT);
  pinMode(dataPin2, OUTPUT);
- 
- //Clear bitmap
- for (int row = 0; row < 8; row++) {
-   for (int zone = 0; zone <= maxZoneIndex; zone++) {
-     bitmap[row][zone] = 0; 
-   }
- }
 }
 
 //FUNCTIONS
 // Displays bitmap array in the matrix
-void RefreshDisplay()
+void RefreshDisplay(int alpha)
 {
- for (int row = 0; row < 8; row++) {
-   int rowbit = 1 << row;
-   digitalWrite(latchPin2, LOW);//Hold latchPin LOW for transmitting data
-   shiftOut(dataPin2, clockPin2, LSBFIRST, rowbit);   //Transmit data
+  for (int row = 0; row < 8; row++) {
+    int rowbit = 1 << row;
+    digitalWrite(latchPin2, LOW);//Hold latchPin LOW for transmitting data
+    shiftOut(dataPin2, clockPin2, LSBFIRST, rowbit);   //Transmit data
 
-   //Start sending column bytes
-   digitalWrite(latchPin1, LOW);//Hold latchPin LOW for transmitting data
-   //Shift out to each matrix
+    //Start sending column bytes
+    digitalWrite(latchPin1, LOW);//Hold latchPin LOW for transmitting data
+   
+    //Shift out to each matrix
+    shiftOut(dataPin1, clockPin1, LSBFIRST, ~alphabets[alpha][row]);
 
-   shiftOut(dataPin1, clockPin1, LSBFIRST, ~alphabets[((x++)/100)%26][row]);
+    //flip both latches at once to eliminate flicker
+    digitalWrite(latchPin1, HIGH);//Return the latch pin 1 high to signal chip
+    digitalWrite(latchPin2, HIGH);//Return the latch pin 2 high to signal chip
 
-   //flip both latches at once to eliminate flicker
-   digitalWrite(latchPin1, HIGH);//Return the latch pin 1 high to signal chip
-   digitalWrite(latchPin2, HIGH);//Return the latch pin 2 high to signal chip
-
-   //Wait
-   delay(10);
- }
+    //Wait
+    delay(100);
+  }
 }
 
 // Converts row and colum to bitmap bit and turn it off/on
-void Plot(int col, int row, bool isOn)
-{
- int zone = col;
- int colBitIndex = x % 8;
- byte colBit = 0 >> colBitIndex;
- if (isOn)
-   bitmap[row][zone] =  bitmap[y][zone] | colBit;
- else
-   bitmap[row][zone] =  bitmap[y][zone] & (~colBit);
-}
-// Plot each character of the message one column at a time, updated the display, shift bitmap left.
 void loop()
 {
- for (int charIndex=0; charIndex < (sizeof(msg)-1); charIndex++)
- {
-   int alphabetIndex = msg[charIndex] - '@';
-   if (alphabetIndex < 0) alphabetIndex=0;
-   
-   //Draw one character of the message
-   // Each character is 5 columns wide, loop two more times to create 2 pixel space betwen characters
-   for (int col = 0; col < 8  ; col++)
-   {
-     for (int row = 0; row < 8; row++)
-     {
-       // Set the pixel to what the alphabet say for columns 0 thru 4, but always leave columns 5 and 6 blank.
-       bool isOn = 0; 
-       if (col>1 || col<5) isOn = bitRead( alphabets[alphabetIndex][col], 7-row ) == 1;
-       Plot( numCols-1, row, isOn); //Draw on the rightmost column, the shift loop below will scroll it leftward.
-     }
-     for (int refreshCount=0; refreshCount < scrollspeed; refreshCount++)
-       RefreshDisplay();
-     //Shift the bitmap one column to left
-     for (int row=0; row<8; row++)
-     {
-       for (int zone=0; zone < numZones; zone++)
-       {
-         //This right shift would show as a left scroll on display because leftmost column is represented by least significant bit of the byte.
-         bitmap[row][zone] = bitmap[row][zone] >> 1;
-         // Shift over lowest bit from the next zone as highest bit of this zone.
-         if (zone < maxZoneIndex) bitWrite(bitmap[row][zone], 8, bitRead(bitmap[row][zone+1],0));
-       }
-     }
-   }
- }
+  index = msg[(x++)%length]-'@';
+  index = index < 0?0:index;
+  for (int blink = 0; blink <= 5; blink++)
+  {
+    RefreshDisplay(index);
+  }
 }
