503eddbe012d8ed7c993d7f67654be8d589880ea
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2021-05-18 16:17:26 +0700

    :scroll: Update custom font.

--- source_code_led_matrix.ino
+++ source_code_led_matrix.ino
@@ -1,6 +1,6 @@
 int x=0;
 int index;
-String msg = "I LOVE COMPRO";//Create word.
+String msg = "I LOVE COMPUTER PROGRAMMING SO MUCH."; //Create word.
 
 //Count character
 int length = msg.length();
@@ -16,36 +16,37 @@ int latchPin2 = 6; //Arduino pin connected to Latch Pin 12 of 74HC595
 int dataPin2 = 7;  //Arduino pin connected to Data Pin 14 of 74HC595
 
 //FONT DEFENITION
-byte alphabets[][8] = {
- {0, 0, 0, 0, 0, 0, 0, 0},//SPACE
- {0, 31, 36, 68, 36, 31},//A
- {0, 127, 73, 73, 73, 54},//B
- {0, 62, 65, 65, 65, 34},//C
- {0, 127, 65, 65, 34, 28},//D
- {0, 127, 73, 73, 65, 65},//E
- {0, 127, 72, 72, 72, 64},//F
- {0, 62, 65, 65, 69, 38},//G
- {0, 127, 8, 8, 8, 127},//H
- {0, 0, 65, 127, 65, 0},//I
- {0, 2, 1, 1, 1, 126},//J
- {0, 127, 8, 20, 34, 65},//K
- {0, 127, 1, 1, 1, 1},//L
- {0, 127, 32, 16, 32, 127},//M
- {0, 127, 32, 16, 8, 127},//N
- {0, 62, 65, 65, 65, 62},//O
- {0, 127, 72, 72, 72, 48},//P
- {0, 62, 65, 69, 66, 61},//Q
- {0, 127, 72, 76, 74, 49},//R
- {0, 50, 73, 73, 73, 38},//S
- {0, 64, 64, 127, 64, 64},//T
- {0, 126, 1, 1, 1, 126},//U
- {0, 124, 2, 1, 2, 124},//V
- {0, 126, 1, 6, 1, 126},//W
- {0, 99, 20, 8, 20, 99},//X
- {0, 96, 16, 15, 16, 96},//Y
- {0, 67, 69, 73, 81, 97},//Z
+byte alphabets[][9] = {
+ {0, 0,     0, 0,     0, 0,     0, 0}, //SPACE
+ {0, 30,   50, 72,   72, 50,   30, 0}, //A
+ {0, 126, 126, 66,   74, 74,   52, 0}, //B
+ {0, 60,   66, 66,   66, 66,  100, 0}, //C
+ {0, 126,  66, 66,   66, 36,   24, 0}, //D
+ {0, 126,  82, 74,   82, 74,   82, 0}, //E
+ {0, 126,  72, 72,   72, 72,  104, 0}, //F
+ {0, 60,   66, 66,   90, 82,   76, 0}, //G
+ {0, 126, 126, 24,   24, 126, 126, 0}, //H
+ {0, 102, 102, 126, 126, 102, 102, 0}, //I
+ {0, 12,   14, 102, 126, 124,  96, 0}, //J
+ {0, 126, 126, 24,   36, 66,   66, 0}, //K
+ {0, 126, 126, 6,     6, 6,    14, 0}, //L
+ {0, 126,  32, 24,   24, 32,  126, 0}, //M
+ {0, 126,  32, 16,    8, 4,   126, 0}, //N
+ {0, 60,   66, 66,   66, 66,   60, 0}, //O
+ {0, 66,  126, 126,  74, 72,   48, 0}, //P
+ {0, 56,   68, 68,   76, 68,   58, 0}, //Q
+ {0, 126, 126, 72,   76, 74,   50, 0}, //R
+ {0, 36,   82, 82,   74, 74,   36, 0}, //S
+ {0, 96,   98, 126, 126, 98,   96, 0}, //T
+ {0, 120, 124, 6,     6, 124, 120, 0}, //U
+ {0, 112,  28, 6,     6, 28,  112, 0}, //V
+ {0, 126,   4, 24,   24, 4,   126, 0}, //W
+ {0, 66,   36, 24,   24, 36,   66, 0}, //X
+ {0, 96,   50, 30,   30, 50,   96, 0}, //Y
+ {0, 98,   70, 74,   82, 98,   70, 0}, //Z
 };
 
+// Function setup program.
 void setup()
 {
  pinMode(latchPin1, OUTPUT);
@@ -57,37 +58,36 @@ void setup()
  pinMode(dataPin2, OUTPUT);
 }
 
-//FUNCTIONS
-// Displays bitmap array in the matrix
-void RefreshDisplay(int alpha)
+// Function output program.
+void Display(int alpha)
 {
   for (int row = 0; row < 8; row++) {
     int rowbit = 1 << row;
-    digitalWrite(latchPin2, LOW);//Hold latchPin LOW for transmitting data
-    shiftOut(dataPin2, clockPin2, LSBFIRST, rowbit);   //Transmit data
+    digitalWrite(latchPin2, LOW); //Hold latchPin LOW for transmitting data
+    shiftOut(dataPin2, clockPin2, LSBFIRST, rowbit); //Transmit data
 
     //Start sending column bytes
-    digitalWrite(latchPin1, LOW);//Hold latchPin LOW for transmitting data
+    digitalWrite(latchPin1, LOW); //Hold latchPin LOW for transmitting data
    
     //Shift out to each matrix
     shiftOut(dataPin1, clockPin1, LSBFIRST, ~alphabets[alpha][row]);
 
     //flip both latches at once to eliminate flicker
-    digitalWrite(latchPin1, HIGH);//Return the latch pin 1 high to signal chip
-    digitalWrite(latchPin2, HIGH);//Return the latch pin 2 high to signal chip
+    digitalWrite(latchPin1, HIGH); //Return the latch pin 1 high to signal chip
+    digitalWrite(latchPin2, HIGH); //Return the latch pin 2 high to signal chip
 
     //Wait
-    delay(100);
+    delay(50);
   }
 }
 
-// Converts row and colum to bitmap bit and turn it off/on
+// Function loop
 void loop()
 {
   index = msg[(x++)%length]-'@';
   index = index < 0?0:index;
-  for (int blink = 0; blink <= 5; blink++)
+  for (int blink = 0; blink <= (index?5:2); blink++)
   {
-    RefreshDisplay(index);
+    Display(index);
   }
 }
